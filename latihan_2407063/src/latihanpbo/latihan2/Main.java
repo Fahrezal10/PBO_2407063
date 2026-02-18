0419a696a5523b16960a9679f672b77c0396c02f
unknown <icalkomp@gmail.com>
2026-02-18 10:44:09 +0700

    latihan2lagi

---
+++ latihan_2407063/src/latihanpbo/latihan2/Main.java
@@ -0,0 +1,23 @@
+package latihanpbo.latihan2;
+
+//acsess modifer (public, private, protected)
+class Persegi{
+    private int sisi;
+
+    public int getSisi() {
+        return sisi*sisi;
+    }
+    public double kelilingPersegi(){
+        return sisi+sisi;
+    }
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Persegi persegi1 = new Persegi();
+        persegi1.setSisi(5);
+        System.out.println("Sisi Persegi: " + persegi1.getSisi());
+    }
+}
