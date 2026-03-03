bce64087f20b973f67f4ec636aefd91dcd418c41
unknown <icalkomp@gmail.com>
2026-03-04 00:31:02 +0700

    kalkulator bangun datar

---
+++ latihan_2407063/src/tugas/tugas2/BangunDatar.java
@@ -0,0 +1,18 @@
+package tugas.tugas2;
+
+public abstract class BangunDatar {
+    private String nama;
+
+    public BangunDatar(String nama) {
+        this.nama = nama;
+    }
+
+    // Getter untuk Encapsulation
+    public String getNama() {
+        return nama;
+    }
+
+    // Abstract method yang WAJIB di-override oleh anak cucunya (Polymorphism)
+    public abstract double hitungLuas();
+    public abstract double hitungKeliling();
+}
