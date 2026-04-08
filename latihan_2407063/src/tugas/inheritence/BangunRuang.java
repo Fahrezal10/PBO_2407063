310a1af7ba3470a4f4b6463226e5111d92531848
unknown <icalkomp@gmail.com>
2026-04-08 08:19:32 +0700

    latihan inheritence

--- latihan_2407063/src/tugas/inheritence/BangunRuang.java
+++ latihan_2407063/src/tugas/inheritence/BangunRuang.java
@@ -1,20 +1,14 @@
 package tugas.inheritence;
 
-// 2. class BangunRuang merupakam turunan dari class BangunDatar
 public class BangunRuang extends BangunDatar {
-    // Memiliki satu atribut yaitu tinggi
     private double tinggi;
 
-    // Memiliki satu constructor
     public BangunRuang(double panjang, double lebar, double tinggi) {
-        // Menggunakan constructor dari superclass (BangunDatar) agar tidak mengulang kode
         super(panjang, lebar);
         this.tinggi = tinggi;
     }
 
-    // Memiliki sebuah method dengan nama hitungVolume
     public double hitungVolume() {
-        // Perhitungan volume diperoleh dari luas (dari class sebelumnya) * tinggi
         return super.hitungLuas() * this.tinggi;
     }
 }
--- latihan_2407063/src/tugas/inheritence/Main.java
+++ latihan_2407063/src/tugas/inheritence/Main.java
@@ -2,22 +2,14 @@ package tugas.inheritence;
 
 public class Main {
     public static void main(String[] args) {
-        // 3. Melakukan pengujian dengan data: 
-        // a. Panjang = 10
-        // b. Lebar = 5
-        // c. Tinggi = 4
         double panjang = 10;
         double lebar = 5;
         double tinggi = 4;
 
-        // Instansiasi objek BangunRuang dengan data yang diberikan
         BangunRuang bangun = new BangunRuang(panjang, lebar, tinggi);
-
-        // 4. Program menampilkan:
-        // a. Luas alas (hasil dari method hitungLuas)
+  
         System.out.println("Luas alas: " +   bangun.hitungLuas());
         
-        // b. Volume (hasil dari method hitungVolume)
         System.out.println("Volume: " + bangun.hitungVolume());
     }
 }
